21f7b9e1624e16a2a8bf4cbf067636f54322efaa
MBS00003 <MBS00003>
2017-02-16 13:16:02 +0300

    remove all

--- age.py
+++
@@ -1,4 +0,0 @@
-age = raw_input(int("How old are you? "))
-height = raw_input(float("How tall are you?"))
-weight = raw_input(float("How much do you weigh?"))
-print"So you are %d old, and %.2f tall and %.f heavy." % (age,height,weight) 
--- mylist.py
+++
@@ -1,7 +0,0 @@
-def sum_even(mylist):
-    mylist =[1,3,5,6,8,10,34,2,0,3]
-    result = 0
-    for i in mylist:
-        if not i % 2:
-            result += i
-            print(result)
